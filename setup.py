8ce3ecbe78a4fdcbe66ddde86c394f395f66e0d8
toinsson <antoine.loriette@gmail.com>
2015-09-04 19:02:16 +0100

    clean setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,3 @@
-# from distutils.core import setup, Extension
-# from Cython.Build import cythonize
-
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
@@ -9,17 +6,14 @@ import numpy
 
 # local path to include resources
 intelPCSDK = r"C:\Program Files (x86)\Intel\PCSDK"
-visualStudio = r"C:\Program Files (x86)\Microsoft Visual Studio 9.0"
-pxcupipeline = r"C:\Users\antoi_000\Documents\dev\senz3d\pxcupipeline"
+pxcupipeline = r"\framework\common\pxcupipeline"
 
 extensions = [
     Extension("pysenz3d", 
         sources = ["pysenz3d.pyx", "senz3d.cpp"],
         include_dirs = [
-                        visualStudio+r"\VC\include",
-                        intelPCSDK+r"\sample\common\include",
                         intelPCSDK+r"\include",
-                        pxcupipeline+r"\include",
+                        intelPCSDK+pxcupipeline+r"\include",
                         numpy.get_include(),
                         ],
         libraries = ['libpxcupipeline_d'],
